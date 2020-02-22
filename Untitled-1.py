6869997d51b05c628baad5bf197b6ae3eaba4492
szy.majorek@gmail.com <szy.majorek@gmail.com>
2020-02-22 18:21:18 +0100

    removing unnecessary stuff

--- Untitled-1.py
+++
@@ -1,5 +0,0 @@
-def calculateDiceC(im1, im2): 
-    im1 = np.asarray(im1).astype(np.bool)
-    im2 = np.asarray(im2).astype(np.bool)
-    intersection = np.logical_and(im1, im2)
-    return 2 * intersection.sum() / (im1.sum() + im2.sum())
